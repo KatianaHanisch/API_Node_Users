0f451fe87096ba806501e4ded9f63cd52b4e8fbe
Katiana Hanisch <katianahanisch@gmail.com>
2024-02-13 16:39:01 -0400

    refactor: adicionar IController comum interface

--- src/controllers/protocols.ts
+++ src/controllers/protocols.ts
@@ -8,3 +8,7 @@ export interface HttpRequest<B> {
   headers?: any;
   body?: B;
 }
+
+export interface IController {
+  handle(httpResquest: HttpRequest<unknown>): Promise<HttpResponse<unknown>>;
+}
